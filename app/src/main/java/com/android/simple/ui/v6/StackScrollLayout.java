639f0b01822d4329f6b6d991d54f88bf4e043e7a
刘勇 <yongliu19@creditease.cn>
2019-12-02 18:29:12 +0800

    StackScrollLayout 完成上滑下滑

--- app/src/main/java/com/android/simple/ui/v6/StackScrollLayout.java
+++ app/src/main/java/com/android/simple/ui/v6/StackScrollLayout.java
@@ -4,11 +4,15 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.simple.R;
@@ -33,6 +37,19 @@ public class StackScrollLayout extends FrameLayout {
      * recyclerView
      */
     private RecyclerView mRecyclerView;
+    /**
+     * 初始recyclerView 上边缘坐标
+     */
+    private int mInitialTop;
+    /**
+     * 手势垂直方向滑动距离，正数向下滑动、负数向上滑动
+     */
+    private int mScrollY;
+    /**
+     * 上一个滑动位置的Y坐标
+     */
+    private int mLastFocusY;
+    private boolean mIsScrollUp = false;
 
     public StackScrollLayout(@NonNull Context context) {
         this(context, null);
@@ -54,8 +71,117 @@ public class StackScrollLayout extends FrameLayout {
     protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
         super.onLayout(changed, left, top, right, bottom);
         MarginLayoutParams anchorMargin = (MarginLayoutParams) mAnchorView.getLayoutParams();
-        final int anchorBottom = mAnchorView.getBottom() + anchorMargin.bottomMargin;
-        mRecyclerView.layout(mRecyclerView.getLeft(), anchorBottom, mRecyclerView.getRight(), mRecyclerView.getBottom());
+        mInitialTop = mAnchorView.getBottom() + anchorMargin.bottomMargin;
+        mRecyclerView.setTranslationY(mInitialTop + mScrollY);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        final int y = (int) (ev.getY() + 0.5f);
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mLastFocusY = y;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                // 如果是垂直滑动
+                if (ev.getY() > ev.getX()) {
+                    int deltaY = y - mLastFocusY;
+                    mScrollY = deltaY;
+                    mLastFocusY = y;
+                    mIsScrollUp = deltaY < 0;
+                    if (deltaY > 0) {
+                        Log.d(TAG, "向下滑动 = " + mScrollY);
+                    } else {
+                        Log.d(TAG, "向上滑动 = " + mScrollY);
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                mLastFocusY = 0;
+                mScrollY = 0;
+                break;
+            default:
+                break;
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean consumed = false;
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (mIsScrollUp) {
+                    consumed = mRecyclerView.getTranslationY() != 0;
+                    final LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+                    if (layoutManager != null && layoutManager.findLastCompletelyVisibleItemPosition() == layoutManager.getItemCount() - 1) {
+                        // recyclerView 内部不能再向上滑动时(加载更多)
+                        consumed = true;
+                    }
+                } else {
+                    final LinearLayoutManager linearLayoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+
+                    if (linearLayoutManager != null) {
+                        consumed = linearLayoutManager.findFirstCompletelyVisibleItemPosition() == 0;
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                consumed = super.onInterceptTouchEvent(ev);
+                break;
+        }
+        Log.d(TAG, "onInterceptTouchEvent consumed = " + consumed + ", action = " + ev.getAction());
+        return consumed;
+    }
+
+    @Override
+    public boolean performClick() {
+        return super.performClick();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        boolean consumed = false;
+        final LinearLayoutManager layoutManager = (LinearLayoutManager) mRecyclerView.getLayoutManager();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                consumed = true;
+            case MotionEvent.ACTION_MOVE:
+                if (mIsScrollUp) {
+                    consumed = mRecyclerView.getTranslationY() != 0;
+                    if (mRecyclerView.getTranslationY() != 0) {
+                        updateRecyclerViewTransitionY(mScrollY);
+                        consumed = true;
+                    } else if (layoutManager != null && layoutManager.findLastCompletelyVisibleItemPosition() == layoutManager.getItemCount() - 1) {
+                        // recyclerView 内部不能再向上滑动时(加载更多)
+                        Log.d(TAG, "加载更多");
+                        consumed = true;
+                    }
+                } else {
+                    if (layoutManager != null && layoutManager.findFirstCompletelyVisibleItemPosition() == 0) {
+                        if (mRecyclerView.getTranslationY() == mInitialTop) {
+                            Log.d(TAG, "下拉刷新");
+                        } else {
+                            updateRecyclerViewTransitionY(mScrollY);
+                        }
+                        consumed = true;
+                    } else {
+                        consumed = false;
+                    }
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                break;
+            default:
+                consumed = super.onInterceptTouchEvent(event);
+                break;
+        }
+        Log.d(TAG, "onTouchEvent consumed = " + consumed + ", action = " + event.getAction());
+        return consumed;
     }
 
     @Override
@@ -76,6 +202,17 @@ public class StackScrollLayout extends FrameLayout {
             mRecyclerView = (RecyclerView) getChildAt(getChildCount() - 1);
         } else {
             throw new IllegalArgumentException(String.format("index %d child must is RecyclerView!", getChildCount() - 1));
+        }
+    }
+
+    private void updateRecyclerViewTransitionY(int deltaY) {
+        final float transitionY = mRecyclerView.getTranslationY();
+        if (transitionY + deltaY >= mInitialTop) {
+            mRecyclerView.setTranslationY(mInitialTop);
+        } else if (transitionY + deltaY <= 0) {
+            mRecyclerView.setTranslationY(0);
+        } else {
+            mRecyclerView.setTranslationY(transitionY + deltaY);
         }
     }
 
